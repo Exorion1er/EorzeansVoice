29fa25f0d247f613daa2fbb50fa5377d0986dd5b
ExoKalork <jason.rivillon@gmail.com>
2021-12-16 20:27:30 +0100

    Game Data helper class

---
+++ GameData.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace EorzeansVoice {
+	public static class GameData {
+		private const string gameProcessName = "ffxiv_dx11";
+		private const ulong posXoffset = 0x1E7AB70;
+		private const ulong posYoffset = 0x1E7AB74;
+		private const ulong posZoffset = 0x1E7AB78;
+
+		private static Process gameProcess;
+
+		public static bool FindProcess() {
+			Process p = Process.GetProcessesByName(gameProcessName).FirstOrDefault();
+			if (p == null) {
+				MessageBox.Show("Couldn't find FFXIV's process.");
+				return false;
+			}
+			gameProcess = p;
+			return true;
+		}
+
+		public static Vector3 GetPosition() {
+			if (gameProcess == null) {
+				return Vector3.Zero;
+			}
+
+			Vector3 pos = Vector3.Zero;
+			pos.x = (float)MemoryReader.GetValue<float>(gameProcess, posXoffset);
+			pos.y = (float)MemoryReader.GetValue<float>(gameProcess, posYoffset);
+			pos.z = (float)MemoryReader.GetValue<float>(gameProcess, posZoffset);
+
+			return pos;
+		}
+	}
+}
---
+++ Vector3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace EorzeansVoice {
+	public class Vector3 {
+		public Vector3(float x, float y, float z) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+
+		public float x;
+		public float y;
+		public float z;
+
+		public static Vector3 Zero { get; } = new Vector3(0f, 0f, 0f);
+
+		public static float Distance(Vector3 a, Vector3 b) {
+			float diff_x = a.x - b.x;
+			float diff_y = a.y - b.y;
+			float diff_z = a.z - b.z;
+			return (float)Math.Sqrt(diff_x * diff_x + diff_y * diff_y + diff_z * diff_z);
+		}
+
+		public override string ToString() {
+			return ToString(null, null);
+		}
+
+		public string ToString(string format) {
+			return ToString(format, null);
+		}
+
+		public string ToString(string format, IFormatProvider formatProvider) {
+			if (string.IsNullOrEmpty(format))
+				format = "F2";
+			if (formatProvider == null)
+				formatProvider = CultureInfo.InvariantCulture.NumberFormat;
+			return string.Format("({0}, {1}, {2})", x.ToString(format, formatProvider), y.ToString(format, formatProvider), z.ToString(format, formatProvider));
+		}
+	}
+}
