1ed86fdef3172b5d107f3af243ea5f80e9cff24d
ExoKalork <jason.rivillon@gmail.com>
2022-01-30 11:23:14 +0100

    Change build script to compile server for linux

--- build.bat
+++ build.bat
@@ -1,27 +1,16 @@
 @echo off
 
 :: Compile
-dotnet publish -r win-x64 --self-contained false -o bin/ -c Release
+cd EorzeansVoice
+dotnet publish -r win-x64 --self-contained false -o ../bin/client/ -c Release
+cd ../EorzeansVoiceServer
+dotnet publish -r win-x64 --self-contained false -o ../bin/server/win -c Release
+dotnet publish -r linux-x64 --self-contained true -o ../bin/server/linux -c Release
+cd ../
 
-:: Folder structure
-mkdir ".\bin\server"
-mkdir ".\bin\client"
-del ".\bin\*.deps.json"
-
-:: Server files
-copy ".\bin\EorzeansVoiceLib.*" ".\bin\server\"
-copy ".\bin\EorzeansVoiceServer.*" ".\bin\server\"
-copy ".\bin\Newtonsoft.Json.dll" ".\bin\server\"
-
-:: Client files
-copy ".\bin\EorzeansVoiceLib.*" ".\bin\client\"
-copy ".\bin\EorzeansVoice.*" ".\bin\client\"
-copy ".\bin\NAudio.*" ".\bin\client\"
-copy ".\bin\Newtonsoft.Json.dll" ".\bin\client\"
-copy ".\bin\opus.dll" ".\bin\client\"
-copy ".\bin\POpusCodec.dll" ".\bin\client\"
-
-:: Cleanup
-del /q ".\bin\*"
+:: Unused files
+del ".\bin\client\*.deps.json"
+del ".\bin\server\win\*.deps.json"
+del ".\bin\server\linux\*.deps.json"
 
 :: TODO : Add Configuration files
