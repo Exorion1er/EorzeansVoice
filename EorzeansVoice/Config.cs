52f1295efef7d740db56029957ba41cebb772ceb
ExoKalork <jason.rivillon@gmail.com>
2022-02-01 11:46:16 +0100

    Basic configuration file controller with upgrading capabilities

--- EorzeansVoice/Config.cs
+++ EorzeansVoice/Config.cs
@@ -1,7 +1,16 @@
-﻿namespace EorzeansVoice {
+﻿using EorzeansVoiceLib;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace EorzeansVoice {
 	public class Config {
+		[JsonIgnore]
 		public const string FileName = "Config.json";
 
+		public int Version { get; set; } = 2;
 		public int ScaleX { get; set; } = 500;
 		public int ScaleY { get; set; } = 650;
 		public string Address { get; set; } = "127.0.0.1";
@@ -10,5 +19,120 @@
 		public bool Deaf { get; set; } = false;
 		public AudioInputProcessing.Mode VoiceMode { get; set; } = AudioInputProcessing.Mode.VoiceActivation;
 		public float VoiceActivationThreshold { get; set; } = 0.7f;
+
+		public static Config Load() {
+			if (!File.Exists(FileName)) {
+				Logging.Warn("Config file doesn't exist.");
+				return CreateDefaultConfig();
+			}
+
+			string content = File.ReadAllText(FileName);
+
+			int v = FindVersion(content);
+			if (v != 0) {
+				Config def = new Config();
+				if (v > def.Version) {
+					Logging.Error("Config file's version is above default version.");
+					MessageBox.Show("An error occurred with your Configuration file. Switching to default Configuraiton.");
+					CorruptedConfig();
+					return CreateDefaultConfig();
+				} else if (v < def.Version) {
+					string upgraded = ApplyUpgrades(v, def.Version, content);
+
+					if (File.Exists("Config.V" + v + ".json")) {
+						File.Delete("Config.V" + v + ".json");
+					}
+					File.Move(FileName, "Config.V" + v + ".json");
+
+					Config config = ParseConfig(upgraded);
+					config.Version = def.Version;
+					config.Save();
+					return config;
+				}
+
+				return ParseConfig(content);
+			}
+			return CreateDefaultConfig();
+		}
+
+		private static Config CreateDefaultConfig() {
+			Logging.Info("Creating new config file.");
+
+			Config config = new Config();
+			config.Save();
+			return config;
+		}
+
+		private static int FindVersion(string text) {
+			Regex versionRGX = new Regex("\"Version\": ([0-9]+),");
+			Match match = versionRGX.Match(text);
+			if (match.Success) {
+				return int.Parse(match.Groups[1].Value);
+			}
+			Logging.Error("Could not find Version in config file.");
+			return 0;
+		}
+
+		private static string ApplyUpgrades(int inputVersion, int targetVersion, string input) {
+			if (inputVersion == targetVersion) {
+				return input;
+			}
+
+			try {
+				switch (inputVersion) {
+					case 1:
+						Regex scaleXRGX = new Regex("\"ScaleX\": ([0-9]+)\\.[0-9]+,");
+						Match matchX = scaleXRGX.Match(input);
+						int scaleX = int.Parse(matchX.Groups[1].Value);
+						input = scaleXRGX.Replace(input, "\"ScaleX\": " + scaleX + ",");
+
+						Regex scaleYRGX = new Regex("\"ScaleY\": ([0-9]+)\\.[0-9]+,");
+						Match matchY = scaleYRGX.Match(input);
+						int scaleY = int.Parse(matchY.Groups[1].Value);
+						input = scaleYRGX.Replace(input, "\"ScaleY\": " + scaleY + ",");
+
+						Logging.Debug("Applied config file upgrade from V1 to V2.");
+						break;
+				}
+			} catch (Exception e) {
+				Logging.Error("Could not apply upgrade to config input : " + input);
+				Logging.Error("Exception : " + e.Message);
+				Logging.Error(e.StackTrace);
+				MessageBox.Show("An error occurred while upgrading your Configuration file. Please restart Eorzeans' Voice.");
+
+				CorruptedConfig();
+				CreateDefaultConfig();
+				Application.Exit();
+				return input;
+			}
+
+			inputVersion++;
+			return ApplyUpgrades(inputVersion, targetVersion, input);
+		}
+
+		private static Config ParseConfig(string input) {
+			try {
+				Config config = JsonConvert.DeserializeObject<Config>(input);
+				return config;
+			} catch (Exception e) {
+				Logging.Error("Could not deserialize Config input : " + input);
+				Logging.Error("Exception : " + e.Message);
+				Logging.Error(e.StackTrace);
+				MessageBox.Show("An error occurred while parsing your Configuration file, switching to default Configuration.");
+
+				CorruptedConfig();
+				return CreateDefaultConfig();
+			}
+		}
+
+		private static void CorruptedConfig() {
+			int rdm = new Random().Next(1, 999999);
+			File.Move(FileName, "Config.CORRUPTED_" + rdm + ".json");
+		}
+
+		public void Save() {
+			string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+			File.WriteAllText(FileName, json);
+		}
 	}
 }
